e13e32ebcfec6a98d796f495d2782beaedf20586
sulino-out <ryansulino.out@gmail.com>
2025-08-21 20:08:51 -0300

    Adicionado a pasta principal do trabalho 1

---
+++ trabalho_1/trabalho_1.c
@@ -0,0 +1,20 @@
+// Nome dos integrantes e número USP
+
+// -
+// -
+// -
+
+// Inclusão de bibliotecas
+#include <stdio.h>
+
+// Protótipo de funções
+
+// Função main
+int main()
+{
+
+
+	return 0;
+}
+
+// Outras funções
